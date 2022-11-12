c5d499ff83ae60532f921114d2881adade4790c1
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-11-12 21:53:51 +0300

    Изменена база данных

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function isFolder(path) {
 
     for (const key of Object.keys(ret)) {
         console.log(key)
-        const dbRef = ref(getDatabase(), `/HiTech/${key}/`);
+        const dbRef = ref(getDatabase(), `OLN-assets/HiTech/${key}/`);
         await set(dbRef, ret[key])
     }
 
